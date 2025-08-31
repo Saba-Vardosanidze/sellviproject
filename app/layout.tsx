b1dba65ee2b8a5fdc35894f1df8ba9552ac26b45
Saba-Vardosanidze <sabavardo674@gmail.com>
2025-08-31 15:40:28 +0400

    feature/scroll bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased custom-scrollbar`}
       >
         <QueryProvider>{children}</QueryProvider>
       </body>
